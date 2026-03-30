b8246abd34498be3ecf02e784096fd8d71ef7a90
sandy-sanjay <sandysanjay589@gmail.com>
2026-03-30 21:10:33 +0530

    initial push: backend and frontend folders

---
+++ HACKATHON_PRACTICE - BACKEND/BACKEND/BACKEND/src/test/java/BACKEND/BACKEND/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package BACKEND.BACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
