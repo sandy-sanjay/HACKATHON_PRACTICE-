ae910faa20effee674de1f7f3e773d20fdf1e6d7
Harish1162004 <harish1162004@gmail.com>
2026-03-30 22:24:19 +0530

    feat: implement Goal & Collaboration module

---
+++ HACKATHON_PRACTICE - BACKEND/BACKEND/BACKEND/src/main/java/BACKEND/BACKEND/goals/model/GoalGroup.java
@@ -0,0 +1,28 @@
+package BACKEND.BACKEND.goals.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "goal_groups")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GoalGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double targetAmount;
+
+    @Column(nullable = false)
+    private Double currentAmount = 0.0;
+}
