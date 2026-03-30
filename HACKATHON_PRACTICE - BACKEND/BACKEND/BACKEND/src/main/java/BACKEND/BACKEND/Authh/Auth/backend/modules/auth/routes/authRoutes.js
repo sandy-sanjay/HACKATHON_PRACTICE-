c077c1bce7b091c317862d2932560184ec5bb774
arulravichandran95 <arulravichandran95@gmail.com>
2026-03-31 00:09:46 +0530

    Revert "Add backend authentication module"

--- HACKATHON_PRACTICE - BACKEND/BACKEND/BACKEND/src/main/java/BACKEND/BACKEND/Authh/Auth/backend/modules/auth/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controller/authController');
-
-router.post('/register', authController.registerUser);
-router.post('/login', authController.loginUser);
-
-module.exports = router;
